d30f6fb9629d6af2546210581971ed35dfc292be
nallas <Samrat@pal-nat186-131-121.itap.purdue.edu>
2018-02-07 18:56:50 -0500

    Familiar with X-code a little

---
+++ Foodie/Foodie/Party.swift
@@ -0,0 +1,31 @@
+//
+//  Party.swift
+//  Foodie
+//
+//  Created by Samrat  on 2/7/18.
+//  Copyright © 2018 Foodie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Party: UIViewController
+{
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
