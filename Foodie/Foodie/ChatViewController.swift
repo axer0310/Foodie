2f0d45da6825ab1f9ce964f85244cee347db4f5f
Youngjoon <youngjoon@Youngjoonui-MacBook-Air.local>
2018-02-10 17:25:29 -0500

    test and created viewer

---
+++ Foodie/Foodie/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  Foodie
+//
+//  Created by Youngjoon Park on 2018. 2. 10..
+//  Copyright © 2018년 Foodie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import GoogleSignIn
+
+
+class LoginViewController: UIViewController {
+    
+    
+
+}
+
