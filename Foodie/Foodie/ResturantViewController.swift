cc8e87216e506665734169d00161c457d68497e0
seohyun <ahn67@purdue.edu>
2018-02-13 23:49:26 -0500

    mapview

--- Foodie/Foodie/ResturantViewController.swift
+++ Foodie/Foodie/ResturantViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 import Firebase
-import GoogleMaps
+import MapKit
+import CoreLocation
 
 class ResturantViewController: UIViewController {
-override func viewDidLoad(){
-    super.viewDidLoad()
-    GMSServices.provideAPIKey("AIzaSyAfqMr44xiniiI-caeIV2bo8lh3z-QfzrE")
-    let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    view = mapView
+    @IBOutlet weak var Map: MKMapView!
     
-    let currentLocation = CLLocationCoordinate2DMake(-33.86, 151.20)
-    let marker = GMSMarker(position: currentLocation)
-    marker.title = "Sydney"
-    marker.map = mapView
-    
-//    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action:"next")
-}
+    override func viewDidLoad(){
+        var location = CLLocationCoordinate2DMake(-33.86,
+                                                  151.20)
+        
+        var span = MKCoordinateSpanMake(0.2, 0.2)
+        var region = MKCoordinateRegion(center: location, span: span)
+        Map.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        annotation.setCoordinate(location);
+        annotation.title = "Sydney"
+        annotation.subtitle="australia"
+        
+        Map.addAnnotation(annotation)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
 //override func loadView() {
 //    // Create a GMSCameraPosition that tells the map to display the
@@ -38,5 +46,18 @@ override func viewDidLoad(){
 //    marker.map = mapView
 //}
 //}
-}
+/*
+ super.viewDidLoad()
+ GMSServices.provideAPIKey("AIzaSyAfqMr44xiniiI-caeIV2bo8lh3z-QfzrE")
+ let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+ let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+ view = mapView
+ 
+ let currentLocation = CLLocationCoordinate2DMake(-33.86, 151.20)
+ let marker = GMSMarker(position: currentLocation)
+ marker.title = "Sydney"
+ marker.map = mapView
+ 
+ //    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action:"next")*/
 
+}
