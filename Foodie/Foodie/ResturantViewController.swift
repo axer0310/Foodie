5753d85f6dd108964938d826395bf7397ecc443f
Youngjoon <park612@purdue.edu>
2018-02-20 18:06:22 -0500

    Merged with MainPage and RestaurantPage

--- Foodie/Foodie/ResturantViewController.swift
+++ Foodie/Foodie/ResturantViewController.swift
@@ -8,35 +8,75 @@
 
 import UIKit
 import Firebase
-import GoogleMaps
+import MapKit
+import CoreLocation
 
-class ResturantViewController: UIViewController {
-override func viewDidLoad(){
-    super.viewDidLoad()
-    GMSServices.provideAPIKey("AIzaSyAfqMr44xiniiI-caeIV2bo8lh3z-QfzrE")
-    let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    view = mapView
+struct Places {
+    let term: String
+    let latitude: Double
+    let longtitude: Double
+    let rating: Double
     
-    let currentLocation = CLLocationCoordinate2DMake(-33.86, 151.20)
-    let marker = GMSMarker(position: currentLocation)
-    marker.title = "Sydney"
-    marker.map = mapView
-    
-//    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action:"next")
-}
-    
-//override func loadView() {
-//    // Create a GMSCameraPosition that tells the map to display the
-//    // coordinate -33.86,151.20 at zoom level 6.
-//    l
-//    // Creates a marker in the center of the map.
-//    let marker = GMSMarker()
-//    marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//    marker.title = "Sydney"
-//    marker.snippet = "Australia"
-//    marker.map = mapView
-//}
-//}
+    init (json: [String: Any]){
+    term = json["term"] as? String ?? ""
+        latitude = json["latitude"] as? Double ?? -1
+        longtitude = json["longtitude"] as? Double ?? -1
+        rating = json["rating"] as? Double ?? -1
+    }
 }
 
+class ResturantViewController: UIViewController , CLLocationManagerDelegate{
+    @IBOutlet weak var Map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var multi_lat1 = -33.86
+    var multi_lon1 = 151.20
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+//        locationManager.delegate = self
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.startUpdatingLocation()
+//        
+//        let jsonUrlString = "https://api.yelp.com/v3/businesses/search"
+//        guard let url = URL(string:jsonUrlString) else { return }
+//        URLSession.shared.dataTask(with: url){
+//            (data, response, err) in
+//
+//            guard let data = data else { return }
+//            let dataString = String(data: data, encoding: .utf8)
+//            print(dataString ?? "")
+//            do {
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
+//
+//                let places = Places(json: json)
+//                print(places.term)
+//            } catch let jsonErr{
+//                print("Error with json ", jsonErr)
+//            }
+//        }.resume()
+//        
+//        //Annotation
+//        let locations : NSMutableArray = []
+//        var location = CLLocationCoordinate2D()
+//        let anno1 = MKPointAnnotation()
+//        
+//        location.latitude = multi_lat1
+//        location.longitude = multi_lon1
+//        anno1.coordinate = CLLocationCoordinate2D(latitude: multi_lat1, longitude: multi_lon1)
+//        locations.add(anno1)
+//        
+//        Map.addAnnotation(locations as! MKAnnotation)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let center = location.coordinate
+        let span = MKCoordinateSpanMake(0.02,  0.02)
+        let region = MKCoordinateRegion(center: center, span: span)
+        Map.setRegion(region, animated: true)
+        Map.showsUserLocation = true
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Foodie/Foodie/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Foodie
+//
+//  Created by Arthur on 2018/2/8.
+//  Copyright © 2018年 Foodie. All rights reserved.
+//
+
+import Foundation
+class User
+{
+    var name : String
+    var gender : String
+    var id : Int
+    
+    init()
+    {
+        name = ""
+        gender=""
+        id = 0
+    }
+}
