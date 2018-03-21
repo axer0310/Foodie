182c9c01acc7528e6a05114dc2661c2a8ab85c9a
pyj2025 <pyj2025@gmail.com>
2018-03-21 15:29:34 -0400

    working

--- Foodie/Foodie/ChatViewController.swift
+++ Foodie/Foodie/ChatViewController.swift
@@ -6,33 +6,90 @@
 //  Copyright © 2018년 Foodie. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import Firebase
+import SnapKit
 import FirebaseDatabase
 import GoogleSignIn
 
-class ChatViewController: UIViewController, UITableViewDelegate {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var array : [UserModel] = []
     
     @IBOutlet weak var chatTitle: UILabel!
+    //@IBOutlet weak var tableView: UITableView!
     
-
-    @IBOutlet weak var tableView: UITableView!
+    var tableView: UITableView
     
-    var ref: DatabaseReference!
-    var partyName: String = ""
-    var location: String = ""
-    var info: String = ""
-    var numPeople: String = "";
-    var x: Double = 40.4278709
-    var y: Double = -86.9169629
-    
+//    var ref: DatabaseReference!
+//    var partyName: String = ""
+//    var location: String = ""
+//    var info: String = ""
+//    var numPeople: String = "";
+//    var x: Double = 40.4278709
+//    var y: Double = -86.9169629
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         //database = firebase.database()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//
+        tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: <#T##String#>,"Cell")
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { (m) in
+            m.top.equalTo(view).offset(20)
+            m.bottom.left.right.equalTo(view)
+        }
+    
+        Database.database().reference().child("Users").observe(DataEventType.value) { (snapshot) in
+            
+            self.array.removeAll()
+            
+            for child in snapshot.children {
+                
+                let fchild = child as! DataSnapShot
+                
+                let userModel = UserModel()
+                
+                userModel.setValuesForKeys(fchild.value as! [String : Any])
+                self.array.append(UserModel )
+            
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData();
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for : indexPath)
+        
+        //URLSession.dataTask(with: URL(string: array[indexPath.row].profileImageURL)
+        
+        let label = UILabel()
+        cell.addSubview(label)
+        label.snp.makeConstraints { (m) in
+            m.centerY.equalTo(cell)
+        }
+        
+        label.text = array[indexPath.row].userName
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,15 +108,8 @@ class ChatViewController: UIViewController, UITableViewDelegate {
         
     }
     
-    @IBAction func qrcode(sender: AnyObject) {
-        // after touching this button
-        // it has to work which relates the qrcode
-        //
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
-        <#code#>
-    }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         <#code#>
---
+++ Foodie/Foodie/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  Foodie
+//
+//  Created by Youngjoon Park on 2018. 3. 21..
+//  Copyright © 2018년 Foodie. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+    var userID :String?
+    var userName :String?
+    
+}
