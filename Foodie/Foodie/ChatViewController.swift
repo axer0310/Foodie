6eae9dc8bbea3ffd9c6ab6b71de8a60e708f6524
pyj2025 <pyj2025@gmail.com>
2018-03-21 20:39:29 -0400

    change local repo test

--- Foodie/Foodie/ChatViewController.swift
+++ Foodie/Foodie/ChatViewController.swift
@@ -8,24 +8,105 @@
 
 import Foundation
 import UIKit
+import SnapKit
 import Firebase
 import GoogleSignIn
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     @IBOutlet weak var chatTitle: UILabel!
     
+    
+    var array : [FriendList] = []
+    
+    
+    var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //database = firebase.database()
+
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+        view.addSubview(tableView)
+
+        tableView.snp.makeConstraints { (m) in
+            m.top.equalTo(view).offset(20)
+            m.bottom.left.right.equalTo(view)
+        }
+
+        Database.database().reference().child("online").observe(DataEventType.value, with: { (snapshot) in
+
+            //self.array.removeAll()
+
+            for child in snapshot.children {
+                let fchild = child as! DataSnapshot
+                let fobject = FriendList()
+
+                fobject.setValuesForKeys(fchild.value as! [String : Any])
+                self.array.append(fobject)
+            }
+
+            DispatchQueue.main.async {
+                self.tableView.reloadData();
+            }
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let imageview = UIImageView()
+        cell.addSubview(imageview)
+        imageview.snp.makeConstraints { (m) in
+            m.centerY.equalTo(cell)
+            m.left.equalTo(cell)
+            m.height.width.equalTo(50)
+        }
+
+        URLSession.shared.dataTask(with: URL(string: array[indexPath.row].profileImageUrl!)!) { (data, response, err) in
+            
+            DispatchQueue.main.async {
+                imageview.image = UIImage(data: data!)
+                imageview.layer.cornerRadius = imageview.frame.size.width/2
+                imageview.clipsToBounds = true
+            }
+            
+        }.resume()
         
-        
-        
+        let label = UILabel()
+        cell.addSubview(label)
+        label.snp.makeConstraints { (m) in
+            m.centerY.equalTo(cell)
+            m.left.equalTo(imageview.snp.right).offset(30)
+        }
+
+        label.text = array[indexPath.row].name
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func BackButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+       
     }
+    
     
     @IBAction func letsParty(sender: AnyObject) {
         // after touching this button
@@ -34,10 +115,6 @@ class ChatViewController: UIViewController {
         
     }
     
-    @IBAction func qrcode(sender: AnyObject) {
-        // after touching this button
-        // it has to work which relates the qrcode
-    }
-    
+  
 }
 
---
+++ Foodie/Foodie/FriendList.swift
@@ -0,0 +1,15 @@
+//
+//  FriendList.swift
+//  Foodie
+//
+//  Created by Youngjoon Park on 2018. 3. 21..
+//  Copyright © 2018년 Foodie. All rights reserved.
+//
+
+import UIKit
+
+class FriendList: NSObject {
+    var profileImageUrl : String?
+    var name : String?
+    
+}
