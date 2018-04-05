7d9d51cc460490c41668463ebeea5646c62f85d5
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2018-04-04 20:50:11 -0400

    Implementing Uber Service

--- Foodie/Foodie/uberViewController.swift
+++ Foodie/Foodie/uberViewController.swift
@@ -9,12 +9,27 @@
 import Foundation
 import UIKit
 import UberRides
+import CoreLocation
 
 class uberViewController: UIViewController
 {
+    
+    let button = RideRequestButton()
+    let locationManager = CLLocationManager()
     override func viewDidLoad()
     {
+        let builder = RideParametersBuilder()
         
+        let pickupLocation = locationManager.location
+        let dropoffLocation = CLLocation(latitude: 37.775200, longitude: -122.417587)
+        builder.pickupLocation = pickupLocation
+        builder.dropoffLocation = dropoffLocation
+        builder.dropoffNickname = "Somewhere"
+        builder.dropoffAddress = "123 Fake St."
+        let rideParameters = builder.build()
+        button.rideParameters = rideParameters
+        button.frame = CGRect(x: 65,y: self.view.frame.size.height/2,width: 250,height:  50)
+        self.view.addSubview(button)
     }
     @IBAction func dismiss(_ sender: Any)
     {
