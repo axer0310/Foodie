0e8a14d43b7ad58f04708815aaf1d52c68b47f52
samrattennis <nallas@purdue.edu>
2018-04-07 18:49:10 -0400

    Finished implementing the tip calculator

--- Foodie/Foodie/ResturantViewController.swift
+++ Foodie/Foodie/ResturantViewController.swift
@@ -76,6 +76,49 @@ class ResturantViewController:UIViewController,MKMapViewDelegate,CLLocationManag
         let region = MKCoordinateRegionMake(location.coordinate, span)
         mapView.setRegion(region, animated: false)
     }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            locationMan.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.1, 0.1)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
+    // add an annotation with an address: String
+    func addAnnotationAtAddress(address: String, title: String, subtitle: String , lat: Double, long: Double, moreInfo: Dictionary<String, Any>) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let placemarks = placemarks {
+                if placemarks.count != 0 {
+                    let coordinate = placemarks.first!.location!
+                    let annotation = Food(coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long))
+//                    annotation.
+//                    annotation.coordinate = coordinate.coordinate
+                    annotation.name = title
+                    annotation.address = subtitle
+                    //[business.name!, business.address!, business.imageURL!, business.distance!, business.rating!, business.reviewCount!]
+                    annotation.imageURL = moreInfo["ImageURL"] as! URL
+                    annotation.distance = moreInfo["Distance"] as! String
+                    annotation.rating = moreInfo["Rating"] as! Double
+                    annotation.reviewCount = moreInfo["reviewCount"] as! Int
+                    annotation.reviewwritten = moreInfo["Reviews"] as! String
+                    if let phone = moreInfo["Phone"] as? String
+                    {
+                        annotation.phone  = phone
+                    }
+//                    annotation.phone = (moreInfo["Phone"] as? String)!
+                    annotation.id = moreInfo["ID"] as! String
+//                    annotation.description.append(id)
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
     
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
